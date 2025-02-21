c7974565ca3e2f25f13fb7bac7312da3b130ca5c
dewaeq <dewaeq@protonmail.com>
2025-02-21 10:34:28 +0100

    better bench

--- src/bench.rs
+++ src/bench.rs
@@ -2,22 +2,22 @@ use std::time::Instant;
 
 use crate::{
     game::Game,
-    players::{random_player::RandomPlayer, Player, PlayerVec},
+    players::{greedy_player::GreedyPlayer, random_player::RandomPlayer, Player, PlayerVec},
 };
 
 pub fn bench(size: Option<usize>) {
     let size = size.unwrap_or(800_000);
-    println!("Simulating {size} random games...");
 
+    run_bench::<RandomPlayer>(size, "random");
+    run_bench::<GreedyPlayer>(size, "greedy");
+}
+
+fn run_bench<T: Player + Default + 'static>(size: usize, name: &str) {
+    println!("Simulating {size} random games for {name}...");
     let mut games = Vec::with_capacity(size);
 
     for _ in 0..size {
-        let players: PlayerVec = vec![
-            RandomPlayer::boxed(),
-            RandomPlayer::boxed(),
-            RandomPlayer::boxed(),
-            RandomPlayer::boxed(),
-        ];
+        let players: PlayerVec = vec![T::boxed(), T::boxed(), T::boxed(), T::boxed()];
 
         games.push(Game::new(players));
     }
@@ -31,17 +31,17 @@ pub fn bench(size: Option<usize>) {
     }
 
     println!(
-        "{}ms, {} games/s",
+        "{name}:\t{}ms, {} games/s",
         start.elapsed().as_millis(),
         (size as f64) / start.elapsed().as_secs_f64()
     );
 
     let mut score = [0; 4];
     for game in &mut games {
-        for i in 0..4 {
-            score[i] += game.score[i];
+        for (i, s) in game.score.iter().enumerate() {
+            score[i] += *s;
         }
     }
 
-    println!("{score:?}");
+    println!("{name}:\t{score:?}");
 }
