02b6d07868b7c92f0adfeb64b8cc94f3b9277d9b
dewaeq <dewaeq@protonmail.com>
2025-02-21 20:41:15 +0100

    experiment with pdep_u64

--- src/bits.rs
+++ src/bits.rs
@@ -1,3 +1,5 @@
+//use std::arch::x86_64::_pdep_u64;
+
 pub const fn lsb(data: u64) -> u64 {
     data.trailing_zeros() as u64
 }
@@ -21,3 +23,18 @@ pub fn select_random_set_bit(mut data: u64) -> u64 {
 
     lsb(data)
 }
+
+// why is this slower?
+// see https://godbolt.org/z/bqjTo8TGE
+// perhaps it's just zen2? https://xcancel.com/trav_downs/status/1202793097962364928#m
+//
+//#[cfg(target_feature = "bmi2")]
+//pub fn select_random_set_bit(data: u64) -> u64 {
+//    let bit = romu::mod_u32(data.count_ones());
+//    let mask = 1 << bit;
+//
+//    unsafe {
+//        let isolated = _pdep_u64(mask, data);
+//        isolated.trailing_zeros() as _
+//    }
+//}
