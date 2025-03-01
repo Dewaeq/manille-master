095edaa6dd83f33b0b5799e7fd3c92ea94d3c137
Dewaeq <dewaeq@protonmail.com>
2025-03-01 11:47:29 +0100

    multithreaded benching (yayy)

--- src/bench.rs
+++ src/bench.rs
@@ -1,9 +1,11 @@
-use std::time::Instant;
+use std::{
+    sync::{Arc, Mutex},
+    time::Instant,
+};
 
 use crate::{
     game::Game,
-    players::{greedy_player::GreedyPlayer, random_player::RandomPlayer, Player, PlayerVec},
-    stack::Stack,
+    players::{mcts_player::MctsPlayer, random_player::RandomPlayer, Player, PlayerVec},
 };
 
 const BENCH_SIZE: usize = 800_000;
@@ -11,18 +13,55 @@ const BENCH_SIZE: usize = 800_000;
 pub fn bench(size: Option<usize>) {
     let size = size.unwrap_or(BENCH_SIZE);
 
-    run_bench::<RandomPlayer>(size, "random");
-    //run_bench::<GreedyPlayer>(size, "greedy");
+    //start_simple_bench::<RandomPlayer>(size, "random");
+    run_bench_multithreaded(10, size, "mcts vs random", false, || {
+        vec![
+            Box::new(MctsPlayer::default().set_search_time(1_000)),
+            RandomPlayer::boxed(),
+            RandomPlayer::boxed(),
+            RandomPlayer::boxed(),
+        ]
+    });
 }
 
-fn run_bench<T: Player + Default + 'static>(size: usize, name: &str) {
+fn start_simple_bench<T: Player + Default + 'static>(size: usize, name: &str) {
+    let player_gen = || -> PlayerVec { vec![T::boxed(), T::boxed(), T::boxed(), T::boxed()] };
+    run_bench(size, name, false, player_gen);
+}
+
+fn run_bench_multithreaded(
+    num_threads: usize,
+    size: usize,
+    name: &str,
+    verbose: bool,
+    player_gen: fn() -> PlayerVec,
+) {
+    let results = Arc::new(Mutex::new([0; 2]));
+
+    std::thread::scope(|s| {
+        for _ in 0..num_threads {
+            let results = Arc::clone(&results);
+
+            s.spawn(move || {
+                let thread_result = run_bench(size, name, verbose, player_gen);
+
+                let mut results = results.lock().unwrap();
+                results[0] += thread_result[0];
+                results[1] += thread_result[1];
+            });
+        }
+    });
+
+    println!("{name}:\t{:?}", results.lock().unwrap());
+}
+
+fn run_bench(size: usize, name: &str, verbose: bool, player_gen: fn() -> PlayerVec) -> [i32; 2] {
     println!("Simulating {size} random games for {name}...");
+
     let mut games = Vec::with_capacity(size);
 
     for _ in 0..size {
-        let players: PlayerVec = vec![T::boxed(), T::boxed(), T::boxed(), T::boxed()];
-
-        games.push(Game::new(players));
+        games.push(Game::new(player_gen()));
     }
 
     let start = Instant::now();
@@ -33,7 +72,10 @@ fn run_bench<T: Player + Default + 'static>(size: usize, name: &str) {
             if game.is_terminal() {
                 break;
             }
-            //game.deal_cards();
+        }
+
+        if verbose {
+            println!("{}", game.winner());
         }
     }
 
@@ -48,5 +90,5 @@ fn run_bench<T: Player + Default + 'static>(size: usize, name: &str) {
         score[game.winner()] += 1;
     }
 
-    println!("{name}:\t{score:?}");
+    score
 }
