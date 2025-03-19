3691fd4ae9fa27b7e402236adac3eb4f3ffc881b
dewaeq <dewaeq@protonmail.com>
2025-03-19 20:47:45 +0100

    gui doesn't run if debug args are provided

--- src/main.rs
+++ src/main.rs
@@ -19,20 +19,23 @@ mod tournament;
 mod trick;
 mod ui;
 
-fn main() {
+fn main() -> eframe::Result {
+    let args: Vec<String> = std::env::args().collect();
     romu::seed();
 
-    let options = eframe::NativeOptions::default();
+    if args.len() == 1 {
+        let options = eframe::NativeOptions::default();
 
-    let app = eframe::run_native(
-        App::name(),
-        options,
-        Box::new(|cc| {
-            egui_extras::install_image_loaders(&cc.egui_ctx);
-            Ok(Box::<App>::default())
-        }),
-    );
-
-    let args: Vec<String> = std::env::args().collect();
-    handle_args(args);
+        eframe::run_native(
+            App::name(),
+            options,
+            Box::new(|cc| {
+                egui_extras::install_image_loaders(&cc.egui_ctx);
+                Ok(Box::<App>::default())
+            }),
+        )
+    } else {
+        handle_args(args);
+        Ok(())
+    }
 }
